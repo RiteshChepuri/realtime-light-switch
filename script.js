0f1b972957b4ce46552edfc5796469175002a002
Ritesh Chepuri <riteshchepuri0@gmail.com>
2024-06-19 20:46:28 +0530

    feat: completed

---
+++ script.js
@@ -0,0 +1,11 @@
+var img = document.querySelector(".img");
+var btn = document.querySelector(".btn1");
+
+function lightOn() {
+  img.style.display = "block";
+  btn.style.display = "block";
+}
+function lightOff() {
+  img.style.display = "none";
+  btn.style.display = "none";
+}
